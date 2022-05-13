50ec499993d056831e6a0a449dfe8b9c248736db
Acer <Acer@MIMAROLU>
2022-05-13 16:30:01 +0300

    Add project files.

---
+++ CoreAndFood/Repositories/CategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using CoreAndFood.Data.Models;
+using CoreAndFood.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAndFood.Repository
+{
+    public class CategoryRepository: GenericRepository<Category>
+    {
+        
+    }
+}
---
+++ CoreAndFood/ViewComponents/FoodListByCategory.cs
@@ -0,0 +1,19 @@
+﻿using CoreAndFood.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAndFood.ViewComponents
+{
+    public class FoodListByCategory: ViewComponent
+    {
+        public IViewComponentResult Invoke(int id)
+        {
+            FoodRepository foodRepository = new FoodRepository();
+            var foodlist = foodRepository.List(x => x.CategoryID == id);
+            return View(foodlist);
+        }
+    }
+}
